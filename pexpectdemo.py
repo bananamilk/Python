a7f1680f9e75afd512504e7efbed3d65808fffcf
Naruto <natsuna@163.com>
2016-02-22 10:01:00 +0800

    PexpectSSHVersion03
    设备地址相同无法选择设备，先改成强制选择1号设备；
    修改斜杠；
    输出执行内容到文件；

--- pexpectdemo.py
+++ pexpectdemo.py
@@ -9,32 +9,36 @@ if __name__ == '__main__':
     ip = '192.168.9.208'
     passwd = '1'
     cmds = ['ps','ls','pwd','ifconfig','date']
-    demo = open("demo.txt", "ab")
-    demo.write ('==========Log Tile: demo==========\n')
-    print user
-    child = pexpect.spawn('ssh %s@%s' % (user,ip))
     try:
-        firstTime = child.expect(['password: ', 'continue connecting (yes/no)?'])
-        if firstTime == 1 :
-            child.sendline('yes')
+        demo = open("demo.txt", "ab")
+        demo.write ('==========Log Tile: demo==========\n')
+        print user
+        child = pexpect.spawn('ssh %s@%s' % (user,ip))
+        
+        # firstTime = child.expect(['password: ', 'continue connecting (yes/no)?'])
+        # if firstTime == 1 :
+        #     child.sendline('yes')
         child.expect('Password:')
         child.sendline(passwd)
         child.expect('Select group:')
         child.sendline('1')
         child.expect('Select page:')
-        child.sendline('16.24')
+        child.sendline('1') #/16.24
         child.expect('Select account:')
-        child.sendline('\root')
+        child.sendline('/root')
         child.expect('Input session comment:')
         child.sendline('natsu')
+        # child.sendline(cmds)
+        # p = pexpect.spawn(cmds)
+        # p.logfile = demo
+        # p.expect(pexpect.EOF)
         for cmd in cmds:
             child.sendline(cmd)
-            print child.before   # Print the result of the ls command.
-            child.logfile = demo
-            child.expect(pexpect.EOF)
+            time.sleep(2)
+            p = pexpect.spawn(cmd)
+            p.logfile = demo
+            p.expect(pexpect.EOF)
         demo.close()
-    except pexpect.EOF:
-        print "EOF"
     except pexpect.TIMEOUT:  
         print "TIMEOUT" 
     finally:
