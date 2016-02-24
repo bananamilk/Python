80a0b3e01e16777d120e504975bc435db42aa349
Naruto <natsuna@163.com>
2016-02-24 11:24:38 +0800

    PexpectSSHVersion05
    把main中的步骤封装到函数sendCommt中

--- pexpectdemo.py
+++ pexpectdemo.py
@@ -3,6 +3,49 @@
 # filename: pexpect_test.py
 
 import pexpect,time,sys
+def sendCommt():
+    demo = open("result.txt", "ab")
+    demo.write('==========Log Tile: demo==========\n')
+    print user
+    child = pexpect.spawn('ssh %s@%s' % (user,ip))
+        
+    # firstTime = child.expect(['password: ', 'continue connecting (yes/no)?'])
+    # if firstTime == 1 :
+    #     child.sendline('yes')
+    while True:
+        i = child.expect(patterns)
+        if i == CONTINUES:#0:
+            child.sendline(flag)
+        elif i == PASSWD:
+            child.sendline(passwd)
+        elif i == GROUP:
+            child.sendline(group)
+        elif i == PAGE:
+            child.sendline('0')
+        elif i == SERVER:
+            child.sendline('16.24')
+        elif i == ACCOUNT:
+            child.sendline('root')
+        elif i == ISCOMMT:
+            child.sendline('natsu')
+        elif i == SERVICE:
+            child.sendline('ssh')
+        elif i == OPFLAG:
+            break
+    # child.sendline(cmds)
+    # p = pexpect.spawn(cmds)
+    # p.logfile = demo
+    # p.expect(pexpect.EOF)
+    for cmd in cmds:
+        # child.sendline(cmd)
+        time.sleep(2)
+        p = pexpect.spawn(cmd)
+        p.logfile = demo
+        p.write('=====================\n')
+        p.expect(pexpect.EOF)
+        print cmd
+    demo.close()
+    child.close()
 
 if __name__ == '__main__':
     user = 'natsu'
@@ -23,49 +66,8 @@ if __name__ == '__main__':
     # OPFLAG = '#'
     # SERVICE = 'Select service:'
     try:
-        demo = open("result.txt", "ab")
-        demo.write('==========Log Tile: demo==========\n')
-        print user
-        child = pexpect.spawn('ssh %s@%s' % (user,ip))
-        
-        # firstTime = child.expect(['password: ', 'continue connecting (yes/no)?'])
-        # if firstTime == 1 :
-        #     child.sendline('yes')
-        while True:
-            i = child.expect(patterns)
-            if i == CONTINUES:#0:
-                child.sendline(flag)
-            elif i == PASSWD:
-                child.sendline(passwd)
-            elif i == GROUP:
-                child.sendline(group)
-            elif i == PAGE:
-                child.sendline('0')
-            elif i == SERVER:
-                child.sendline('16.24')
-            elif i == ACCOUNT:
-                child.sendline('root')
-            elif i == ISCOMMT:
-                child.sendline('natsu')
-            elif i == SERVICE:
-                child.sendline('ssh')
-            elif i == OPFLAG:
-                break
-        # child.sendline(cmds)
-        # p = pexpect.spawn(cmds)
-        # p.logfile = demo
-        # p.expect(pexpect.EOF)
-        for cmd in cmds:
-            # child.sendline(cmd)
-            time.sleep(2)
-            p = pexpect.spawn(cmd)
-            p.logfile = demo
-            p.write('=====================\n')
-            p.expect(pexpect.EOF)
-        demo.close()
+        sendCommt()
     except pexpect.TIMEOUT:  
         print "TIMEOUT" 
     # except pexpect.EOF:
     #     print "EOF"
-    finally:
-        child.close()
