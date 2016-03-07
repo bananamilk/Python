07ab8e46b4812e436037901189254145d8013b09
Naruto <natsuna@163.com>
2016-03-07 18:21:56 +0800

    循环读取用户名并连接
    连接ldap的时候会报错
    初步怀疑readline读取的内容带了格式

--- ldapdemo.py
+++ ldapdemo.py
@@ -8,28 +8,40 @@ import ldap
 '''
 
 #ldap用户登录
-def logonAnnymous():
+# def logonAnnymous():
+# 	l = ldap.initialize(ldapPath)
+# 	# Set LDAP protocol version used
+# 	l.protocol_version = ldap.VERSION3
+# 	l.simple_bind_s(ldapUser,ldapPasswd)
+# 	# l.simple_bind_s(dn,ldapPasswd) 
+# 	print "bind success"
+
+#获得用户的dn
+def getLdapUserDN():
 	l = ldap.initialize(ldapPath)
 	# Set LDAP protocol version used
 	l.protocol_version = ldap.VERSION3
 	l.simple_bind_s(ldapUser,ldapPasswd)
 	# l.simple_bind_s(dn,ldapPasswd) 
 	print "bind success"
-
-#获得用户的dn
-def getLdapUserDN(user):
-	searchScope = ldap.SCOPE_SUBTREE
-	
-
-
+		
 
 if __name__ == '__main__':
 	# ldapPath = "ldap://10.10.16.14"
 	ldapPath = "ldap://10.10.16.12"
-	baseDn = "DC=test,DC=com"
+	baseDN = "DC=test,DC=com"
 	# ldapUser = "root"
 	ldapUser = "admin"
 	ldapPasswd = "shterm"
+	# demo = "test6"
 	# dn = "uid=root,ou=People,dc=shterm,dc=com"
-	logonAnnymous()
+	# logonAnnymous()
 	
+	file = open("test.txt")
+	while 1:
+	    line = file.readline()
+	    ldapUser = line
+	    getLdapUserDN()
+	    if not line:
+	        break
+	file.close()
