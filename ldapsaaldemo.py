9554af2e29c1b17da5d5bb8886470560b08085e8
Naruto <natsuna@163.com>
2016-04-15 14:29:08 +0800

    ldap-sasl

--- ldapsaaldemo.py
+++ ldapsaaldemo.py
@@ -2,26 +2,33 @@
 # -*- coding: utf-8 -*-
 # filename: ldap_test.py
 import ldap,ldap.sasl
+import socket
 
 '''
 实现LDAP用户登录验证SAAL
 '''
-if __name__ == '__main__' :
-	ldap.sasl._trace_level=0
 
-	ldap.set_option(ldap.OPT_DEBUG_LEVEL,0)
+ldap.sasl._trace_level=0
 
-	ldapPath = "ldap://10.10.16.11"
-	sasl_mech = "DIGEST-MD5"
-	sasl_cb_value_dict =  {
-      ldap.sasl.CB_AUTHNAME    :'test1',
-      ldap.sasl.CB_PASS        :'0',
-    }
+ldap.set_option(ldap.OPT_REFERRALS,0)
 
-	sasl_auth = ldap.sasl.sasl(sasl_cb_value_dict,sasl_mech)
-	print 20*'*',sasl_auth.mech,20*'*'
+ipaddr = "demo"
+ldapPath = "ldap://" + socket.gethostbyname(ipaddr) + ":389" 
+
+method = "DIGEST-MD5"
+username = 'test1'.encode('utf-8')
+password = '0'
+sasl_dict = {ldap.sasl.CB_AUTHNAME:username, ldap.sasl.CB_PASS:password}
+try:
 	l = ldap.initialize(ldapPath,trace_level=0)
 	l.protocol_version = 3
-	l.sasl_interactive_bind_s("", sasl_auth)
-	print 'OPT_X_SASL_USERNAME',repr(l.get_option(ldap.OPT_X_SASL_USERNAME))
+
+	sasl_auth = ldap.sasl.sasl(sasl_dict, method)
+	print 20*'*',sasl_auth.mech,20*'*'
+	print sasl_auth
+	l.sasl_interactive_bind_s('', sasl_auth)
+#print 'OPT_X_SASL_USERNAME',repr(l.get_option(ldap.OPT_X_SASL_USERNAME))
+except ldap.INVALID_CREDENTIALS, e:
+	print e
+finally:
 	l.unbind()
